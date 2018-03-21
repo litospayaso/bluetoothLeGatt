6754da3ef50b5c2df20b398c070726c88f9d2b28
angel.hita <angel.hita@netexlearning.com>
2018-03-21 17:41:36 +0100

    adding all tweets in csv

--- replace/exportJson.sh
+++ replace/exportJson.sh
@@ -1,5 +1,5 @@
 for f in *.json; do 
-    echo $f
-    echo "${f:0:10}.csv"
-    # json2csv -i $f -o "${f:0:10}.csv"
+    # echo $f
+    # echo "${f:0:10}.csv"
+    json2csv -i $f -o "${f:0:10}.csv"
 done
